b3bb3a50ceb0e6be1e3b605a5f8b24803f8edce4
jalajkhanna <jalaj.khanna@gmail.com>
2018-11-06 23:30:27 -0500

    update profile

--- src/main/java/com/windsor/foodapp/controller/LoggedInUserController.java
+++ src/main/java/com/windsor/foodapp/controller/LoggedInUserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 //
 @RestController
@@ -25,6 +27,27 @@ public class LoggedInUserController {
     public String getUserInfo(@RequestParam("email") String email, @RequestParam("token") String token) throws JsonProcessingException {
         ClientUser userForEmail =userService.getUserForEmail(email);
         return objectMapper.writeValueAsString(userForEmail);
+    }
+
+    //Assuming email to be same and rest of the paramaters can be present or not
+    @RequestMapping(value = "/updateProfile", method = RequestMethod.POST)
+    public String updateUserProfile(@RequestParam("email") String email,
+                                    @RequestParam("token") String token,
+                                    @RequestParam(value = "firstName", required = false) String firstName,
+                                    @RequestParam(value = "lastName", required = false) String lastName ,
+                                    @RequestParam(value = "password", required = false) String password,
+                                    @RequestParam(value = "phoneNum", required = false) String phoneNum ) throws JsonProcessingException {
+        Map<String, String> resultMap = new HashMap<>();
+
+        try {
+            userService.updateProfile(email, firstName, lastName, phoneNum, password);
+            resultMap.put("status", "success");
+        } catch (Exception e) {
+            resultMap.put("status", "failed");
+            resultMap.put("reason",e.getMessage());
+        } finally {
+            return objectMapper.writeValueAsString(resultMap);
+        }
     }
 
 }
